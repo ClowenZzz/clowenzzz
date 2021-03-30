0c68c7fa8e80c12d7fef5072decc0fe0e55df772
root <473075311@qq.com>
2021-03-30 17:06:50 +0800

    添加了springboot

---
+++ springboot1/src/main/java/Controller/springbootCon.java
@@ -0,0 +1,17 @@
+package Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class springbootCon {
+  
+  @RequestMapping("/hello")
+  @ResponseBody
+  public String msg(){
+	  return "hello spring boot";
+  }
+}
